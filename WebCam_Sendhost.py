12ca9921eaea09607e4c7ec2efeda39f8951ba2a
Uszak1 <wiktor.zarzyka@gmail.com>
2024-12-27 20:28:53 +0100

    Update WebCam_Sendhost.py

--- WebCam_Sendhost.py
+++ WebCam_Sendhost.py
@@ -1,43 +1,35 @@
-from flask import Flask, Response 
+from flask import Flask, Response
 import cv2
 
 app = Flask(__name__)
 
-def find_cameras():
-    available_cameras = []
-    for i in range(1):
-        camera = cv2.VideoCapture(i, cv2.CAP_DSHOW)
-        if camera.isOpened():
-            available_cameras.append(i)
-            camera.release()
-    return available_cameras
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-cameras = find_cameras()
-if cameras:
-    last_camera_index = cameras[-1]
-    camera = cv2.VideoCapture(last_camera_index, cv2.CAP_DSHOW)
-    if not camera.isOpened():
-        exit()
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    camera.set(cv2.CAP_PROP_FPS, 30)
-else:
-    exit()
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(3, 640), cap.set(4, 480), cap.set(5, 80)
 
+if not cap.isOpened():
+    exit("Couldn't open camera.")
+    
 def generate_frames():
     while True:
-        success, frame = camera.read()
-        if not success:
+        ret, frame = cap.read()
+        if not ret:
             break
-        else:
-            _, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        frame = cv2.flip(frame, 1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        for i, (x, y, w, h) in enumerate(faces, 1):
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.putText(frame, f'Face {i}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
 @app.route('/')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4545)
